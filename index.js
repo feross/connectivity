7048a98442e0881268d393656e5df4c240678bdd
Feross Aboukhadijeh <feross@feross.org>
2016-10-10 15:31:21 -0700

    Use example.com instead of google.com
    Because google.com does not work in China.
    
    Fixes: https://github.com/feross/connectivity/issues/1

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var once = require('once')
 module.exports = function (cb) {
   var socket = net.connect({
     port: 80,
-    host: 'google.com'
+    host: 'example.com'
   })
 
   // If no 'error' or 'connect' event after 5s, assume network is down
